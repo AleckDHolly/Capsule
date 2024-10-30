502af0a56f0a13a79c21c601849d428bb4d72642
AleckDHolly <aleckdavidholly@gmail.com>
2024-10-30 18:46:30 -0400

    Initial Commit

---
+++ Capsule/CapsuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapsuleApp.swift
+//  Capsule
+//
+//  Created by Aleck David Holly on 2024-10-30.
+//
+
+import SwiftUI
+
+@main
+struct CapsuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Capsule/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Capsule
+//
+//  Created by Aleck David Holly on 2024-10-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
